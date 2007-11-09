90ebc0c816a28923871ec1428c52e42dc00c4729
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:55:17 +0100

    - Unify x86 mdb declarations

---
+++ kernel/src/glue/v4-x86/mdb.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     glue/v4-x86/mdb.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __GLUE__V4_X86__MDB_H__
+#define __GLUE__V4_X86__MDB_H__
+
+#include INC_GLUE_SA(mdb.h)
+
+#endif /* !__GLUE__V4_X86__MDB_H__ */
