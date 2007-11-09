8b6b794478648d54f989a60d182d22b85d890d83
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:25:15 +0100

    - ARCHX not needed anymore

--- kernel/src/platform/pc99/8259.h
+++ kernel/src/platform/pc99/8259.h
@@ -32,7 +32,7 @@
 #ifndef __PLATFORM__PC99__8259_H__
 #define __PLATFORM__PC99__8259_H__
 
-#include INC_ARCHX(x86,ioport.h)	/* for in_u8/out_u8	*/
+#include INC_ARCH(ioport.h)	/* for in_u8/out_u8	*/
 
 /**
  * Driver for i8259 PIC
