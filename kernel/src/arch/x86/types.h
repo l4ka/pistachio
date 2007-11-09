20a18b86bdf221a36bad5aa437843cf0139ddce5
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:43:18 +0100

    - x86-generic types placeholder

---
+++ kernel/src/arch/x86/types.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     arch/x86/types.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __ARCH__X86__TYPES_H__
+#define __ARCH__X86__TYPES_H__
+
+#include INC_ARCH_SA(types.h)
+
+#endif /* !__ARCH__X86__TYPES_H__ */
