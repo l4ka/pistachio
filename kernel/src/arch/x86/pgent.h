214975a6a576ef57200fbf3f2e2b2a832ed8f99a
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:42:27 +0100

    -  x86 pgent definition placeholder

---
+++ kernel/src/arch/x86/pgent.h
@@ -0,0 +1,17 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     arch/x86/pgent.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __ARCH__X86__PGENT_H__
+#define __ARCH__X86__PGENT_H__
+
+#include INC_ARCH_SA(pgent.h)
+#endif /* !__ARCH__X86__PGENT_H__ */
