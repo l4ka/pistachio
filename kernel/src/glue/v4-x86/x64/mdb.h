95592a0f830e23730e3b8ce5d2f50bfdd6b69556
Jan Stoess <stoess@ira.uka.de>
2007-11-09 16:06:15 +0100

    - Moved x64-specific mdb declarations

---
+++ kernel/src/glue/v4-x86/x64/mdb.h
@@ -0,0 +1,40 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2005,  Karlsruhe University
+ *                
+ * File path:     glue/v4-amd64/mdb.h
+ * Description:   MDB configuration for IA32
+ *                
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *                
+ * $Id: mdb.h,v 1.1 2005/05/18 14:54:13 stoess Exp $
+ *                
+ ********************************************************************/
+#ifndef __GLUE_V4_X86__X64__MDB_H__
+#define __GLUE_V4_X86__X64__MDB_H__
+
+#define MDB_MEM_SIZES            { 12, 21, 30, 39, 48 }
+#define MDB_MEM_NUMSIZES	4
+
+
+
+#endif /* !__GLUE_V4_X86__X64__MDB_H__ */
