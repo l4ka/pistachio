72345aa314774f8bb0a470ddae801ef3cd19d01a
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:54:49 +0100

    - Placeholder for x86 ktcb

---
+++ kernel/src/glue/v4-x86/ktcb.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     glue/v4-x86/ktcb.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __GLUE__V4_X86__KTCB_H__
+#define __GLUE__V4_X86__KTCB_H__
+
+#include INC_GLUE_SA(ktcb.h)
+
+#endif /* !__GLUE__V4_X86__KTCB_H__ */
