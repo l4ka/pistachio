b69ea27b47fc4695ddbf3f9bafd09605a463f068
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:57:03 +0100

    - Placeholder for x86 syscall declarations

---
+++ kernel/src/glue/v4-x86/syscalls.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     glue/v4-x86/syscalls.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __GLUE__V4_X86__SYSCALLS_H__
+#define __GLUE__V4_X86__SYSCALLS_H__
+
+#include INC_GLUE_SA(syscalls.h)
+
+#endif /* !__GLUE__V4_X86__SYSCALLS_H__ */
