e2de8b5844e7973ffaaf4ece9d1253be2d7d02f7
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:53:49 +0100

    - Placeholder for x86 hwirq declarations

---
+++ kernel/src/glue/v4-x86/hwirq.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     glue/v4-x86/hwirq.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __GLUE__V4_X86__HWIRQ_H__
+#define __GLUE__V4_X86__HWIRQ_H__
+
+#include INC_GLUE_SA(hwirq.h)
+
+#endif /* !__GLUE__V4_X86__HWIRQ_H__ */
