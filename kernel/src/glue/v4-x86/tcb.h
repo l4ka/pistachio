23e670e86badcba46e05c85ddba151b72ff6e097
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:57:16 +0100

    - Placeholder for x86 tcb declarations

---
+++ kernel/src/glue/v4-x86/tcb.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     glue/v4-x86/tcb.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __GLUE__V4_X86__TCB_H__
+#define __GLUE__V4_X86__TCB_H__
+
+#include INC_GLUE_SA(tcb.h)
+
+#endif /* !__GLUE__V4_X86__TCB_H__ */
