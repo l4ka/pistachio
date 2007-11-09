7e75af0aedb1b9cc823a3016e8e22da9e35c1288
Jan Stoess <stoess@ira.uka.de>
2007-11-09 15:56:49 +0100

    - Placeholder for x86 space declarations

---
+++ kernel/src/glue/v4-x86/space.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2007,  Karlsruhe University
+ *                
+ * File path:     glue/v4-x86/space.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __GLUE__V4_X86__SPACE_H__
+#define __GLUE__V4_X86__SPACE_H__
+
+#include INC_GLUE_SA(space.h)
+
+#endif /* !__GLUE__V4_X86__SPACE_H__ */
