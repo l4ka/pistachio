445569119dd6484fa28aae3fee689a43e4a3e69b
Jan Stoess <stoess@ira.uka.de>
2007-11-26 12:26:00 +0100

    - Move x86-generic MMU bit definition to glue/v4-x86/x86.h - Rename AMD64_PAGE -> X86_PAGE and AMD64_SPAGE -> X86_SUPERPAGE

--- kernel/src/arch/x86/x64/x86.h
+++ kernel/src/arch/x86/x64/x86.h
@@ -35,11 +35,78 @@
 #include INC_ARCH(x86.h)
 
 
-/* exception error code bits */
+/**********************************************************************
+ *    MMU
+ **********************************************************************/
+
+/* Sign extend 63..48 */
+#define AMD64_SIGN_EXTEND_BITS   48
+#define AMD64_SIGN_EXTEND_SIZE   (X86_64BIT_ONE << AMD64_SIGN_EXTEND_BITS)
+#define AMD64_SIGN_EXTEND_MASK   (~(AMD64_SIGN_EXTEND_SIZE - 1))
+#define AMD64_SIGN_EXTENSION	 (~(AMD64_SIGN_EXTEND_SIZE - 1))
+
+/* Page map 47.. 39 */
+#define AMD64_PML4_BITS		39
+#define AMD64_PML4_SIZE         (X86_64BIT_ONE << AMD64_PML4_BITS)
+#define AMD64_PML4_MASK         ((~(AMD64_PML4_SIZE - 1)) ^ (~(AMD64_SIGN_EXTEND_SIZE - 1)))
+#define AMD64_PML4_IDX(x)	((x & AMD64_PML4_MASK) >> AMD64_PML4_BITS)
+
+/* Page directory pointer 38..30 */
+#define AMD64_PDP_BITS           30
+#define AMD64_PDP_SIZE           (X86_64BIT_ONE << AMD64_PDP_BITS)
+#define AMD64_PDP_MASK           ((~(AMD64_PDP_SIZE - 1))  ^ (~(AMD64_PML4_SIZE - 1)))
+#define AMD64_PDP_IDX(x)	 ((x & AMD64_PDP_MASK) >> AMD64_PDP_BITS)
+
+/* Page directory 29..21 */
+#define AMD64_PDIR_BITS         21
+#define AMD64_PDIR_SIZE         (X86_64BIT_ONE << AMD64_PDIR_BITS)
+#define AMD64_PDIR_MASK         ((~(AMD64_PDIR_SIZE - 1))  ^ (~(AMD64_PDP_SIZE - 1)))
+#define AMD64_PDIR_IDX(x)	((x & AMD64_PDIR_MASK) >> AMD64_PDIR_BITS)
+
+/* Pagetable 20..12  */
+#define AMD64_PTAB_BITS          12
+#define AMD64_PTAB_SIZE          (X86_64BIT_ONE << AMD64_PTAB_BITS)
+#define AMD64_PTAB_MASK          ((~(AMD64_PTAB_SIZE - 1))  ^ (~(AMD64_PTAB_SIZE - 1)))
+#define AMD64_PTAB_IDX(x)        ((x & AMD64_PTAB_MASK) >> AMD64_PTAB_BITS)
+
+
+#define X86_PAGE_CPULOCAL       (1<<9)
+#define X86_PAGE_NX		(1<<63)
+
+/**
+ * 
+ * Bits to zero out invalid parts of pagetable entries 
+ */
+
+/* Normal pagetable entry 11..0  */
+#define AMD64_PTE_BITS			12
+#define AMD64_PTE_SIZE			(X86_64BIT_ONE << AMD64_PTE_BITS) 
+#define AMD64_PTE_MASK			(~(AMD64_PTE_SIZE - 1))
+#define AMD64_PTE_FLAGS_MASK		(0x0e3f)
+
+/* pagefault error code bits */
 #define AMD64_PF_RW	(1 << 1)	/* Pagefault on read/write	*/
 #define AMD64_PF_US	(1 << 2)	/* Pagefault in user/kernel	*/
 #define AMD64_PF_ID	(1 << 4)	/* Pagefault on insn./data	*/
 
+/* 2 MByte (Super-) Pages 20..0  */
+#define X86_SUPERPAGE_BITS		21
+#define X86_SUPERPAGE_SIZE		(__UL(1) << X86_SUPERPAGE_BITS) 
+#define X86_SUPERPAGE_MASK		(~(X86_SUPERPAGE_SIZE - 1))
+#define X86_SUPERPAGE_FLAGS_MASK	(0x1fff)
+
+#define X86_PAGE_FLAGS_MASK		(0x0fff)
+
+#define X86_PAGEFAULT_BITS		(AMD64_PF_RW | AMD64_PF_ID)
+
+#define X86_TOP_PDIR_BITS		AMD64_PML4_BITS
+#define X86_TOP_PDIR_SIZE		AMD64_PML4_SIZE
+#define X86_TOP_PDIR_IDX(x)		AMD64_PML4_IDX(x)
+
+
+/**********************************************************************
+ *    CPU features (CPUID)  
+ **********************************************************************/
 
 /* extended feature register (EFER) bits */
 #define AMD64_EFER_SCE  (1 <<  0)       /* system call extensions       */
@@ -47,9 +114,10 @@
 #define AMD64_EFER_LMA  (1 << 10)       /* long mode active             */
 #define AMD64_EFER_NXE  (1 << 11)       /* nx bit enable                */
 
-/*
+/**********************************************************************
  * Model specific register locations.
- */
+ **********************************************************************/
+
 #define AMD64_MCG_CAP_MSR               0x0179  /* Machine Check Global Capabilities */
 #define AMD64_MCG_STATUS_MSR            0x0179  /* Machine Check Global Status  */
 #define AMD64_MCG_CTL_MSR               0x0179  /* Machine Check Global Control  */
@@ -85,7 +153,10 @@
 #define AMD64_MTRRBASE(x)               (0x200 + 2*(x) + 0)
 #define AMD64_MTRRMASK(x)               (0x200 + 2*(x) + 1)
 
-/* if cpuid does not provide it, we use this value */
+/**********************************************************************
+ *   Cache line configurations
+ **********************************************************************/
+
 #define AMD64_CACHE_LINE_SIZE          64
 
 
