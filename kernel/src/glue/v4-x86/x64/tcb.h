6a0ce2490e6994e5b9a2b01d60fe343841b49875
Jan Stoess <stoess@ira.uka.de>
2007-11-26 14:08:20 +0100

    - Move x86-generic tcb handling to v4-x86/tcb.h

--- kernel/src/glue/v4-x86/x64/tcb.h
+++ kernel/src/glue/v4-x86/x64/tcb.h
@@ -32,206 +32,41 @@
 #ifndef __GLUE_V4_X86__X64__TCB_H__
 #define __GLUE_V4_X86__X64__TCB_H__
 
-#ifndef __API__V4__TCB_H__
-#error not for stand-alone inclusion
-#endif
-
-
-#include INC_API(syscalls.h)			/* for sys_ipc */
 #include INC_ARCH_SA(tss.h)			/* for amd64_tss_t */
-#include INC_GLUE_SA(resource_functions.h)	/* for thread_resources_t */
 
 #if defined(CONFIG_X86_COMPATIBILITY_MODE)
-
 #include INC_GLUE_SA(x32comp/tcb.h)
+#else 
 
-#else /* !defined(CONFIG_X86_COMPATIBILITY_MODE) */
-
-/**
- * sets the CPU
- * @param cpuid
- */
-INLINE void tcb_t::set_cpu(cpuid_t cpu)
-{
-    this->cpu = cpu;
-
-    // only update UTCB if there is one
-    if (get_utcb())
-        get_utcb()->processor_no = cpu;
-
-    // update the pdir cache on migration
-    if (this->space)
-        this->pdir_cache = (word_t)space->get_pagetable(get_cpu());
-
-}
 
 /**
- * read value of message register
- * @param index number of message register
+ * copies a set of message registers from one UTCB to another
+ * @param dest destination TCB
+ * @param start MR start index
+ * @param count number of MRs to be copied
  */
-INLINE word_t tcb_t::get_mr(word_t index)
-{
-    return get_utcb()->mr[index];
-}
-
-/*
- * set the value of a message register
- * @param index number of message register
- * @param value value to set
- */
-INLINE void tcb_t::set_mr(word_t index, word_t value)
-{
-    get_utcb()->mr[index] = value;
-}
-
-/**
-  * copies a set of message registers from one UTCB to another
-  * @param dest destination TCB
-  * @param start MR start index
-  * @param count number of MRs to be copied
-  */
 INLINE void tcb_t::copy_mrs(tcb_t * dest, word_t start, word_t count)
 {
-     ASSERT(start + count <= IPC_NUM_MR);
-     ASSERT(count > 0);
+    ASSERT(start + count <= IPC_NUM_MR);
+    ASSERT(count > 0);
+    word_t dummy;
 
-     word_t dummy;
-     /* use optimized AMD64 copy loop -- uses complete cacheline
-        transfers */
-     __asm__ __volatile__ (
-         "repnz movsq (%%rsi), (%%rdi)\n"
-         : /* output */
-	 "=S"(dummy), "=D"(dummy), "=c"(dummy)
-         : /* input */
-         "c"(count), "S"(&get_utcb()->mr[start]),
-         "D"(&dest->get_utcb()->mr[start]));
+    /* use optimized IA32 copy loop -- uses complete cacheline
+       transfers */
+    __asm__ __volatile__ (
+	"cld\n"
+	"rep  movsq (%0), (%1)\n"
+	: /* output */
+	"=S"(dummy), "=D"(dummy), "=c"(dummy)
+	: /* input */
+	"c"(count), "S"(&get_utcb()->mr[start]), 
+	"D"(&dest->get_utcb()->mr[start]));
+
 }
 
-/**
- * read value of buffer register
- * @param index number of buffer register
- */
-INLINE word_t tcb_t::get_br(word_t index)
-{
-    return get_utcb()->br[32U-index];
-}
-
-
-/**
- * set the value of a buffer register
- * @param index number of buffer register
- * @param value value to set
- */
-INLINE void tcb_t::set_br(word_t index, word_t value)
-{
-    get_utcb()->br[32U-index] = value;
-}
 
 #endif /* !defined(CONFIG_X86_COMPATIBILITY_MODE) */
 
-
-INLINE void tcb_t::set_utcb_location(word_t utcb_location)
-{
-    /* utcb address points to mr0 at offset 0x200 */
-    myself_local.set_raw (utcb_location + 0x200);
-}
-
-INLINE word_t tcb_t::get_utcb_location()
-{
-#if defined(CONFIG_X86_COMPATIBILITY_MODE)
-    /*
-     * srXXX: This is rather ugly!
-     * To create a 32-bit thread in a new address space, an application
-     * must call ThreadControl without a pager, then SpaceControl with
-     * appropriate flags, then ThreadControl with a pager.
-     * The ThreadControl implementation will call space_t::allocate_utcb,
-     * which is implemented in glue, and the AMD64 implementation will
-     * call this function. Therefore we update the resource bits here.
-     * To make this a little less ugly, it should be done in
-     * allocate_utcb, but unfortunately the resource bits are private.
-     * The correct way to implement this would be to maintain a list of
-     * TCBs in every address space, and update all of these in
-     * space_control.
-     */
-    if (get_space() && get_space()->is_compatibility_mode())
-	resource_bits += COMPATIBILITY_MODE;
-#endif /* !defined(CONFIG_X86_COMPATIBILITY_MODE) */
-
-    return myself_local.get_raw() - 0x200;
-}
-
-
-/**
- * allocate the tcb
- * The tcb pointed to by this will be allocated.
- */
-INLINE void tcb_t::allocate()
-{
-    __asm__ __volatile__(
-	"orq $0, %0\n"
-	:
-	: "m"(*this));
-
-}
-
-
-/**
- * set the address space a TCB belongs to
- * @param space address space the TCB will be associated with
- */
-INLINE void tcb_t::set_space(space_t * space)
-{ 
-    //TRACEF("tcb=%t, space=%p\n", this, space);
-    this->space = space;
-
-    if (space)
-    {
-	this->pdir_cache = (word_t)space->get_pagetable();
-
-#if defined(CONFIG_X86_COMPATIBILITY_MODE)
-	if (space->is_compatibility_mode())
-	    resource_bits += COMPATIBILITY_MODE;
-#endif /* defined(CONFIG_X86_COMPATIBILITY_MODE) */
-    }
-}
-
-
-/**
- * Short circuit a return path from an IPC system call.  The error
- * code TCR and message registers are already set properly.  The
- * function only needs to restore the appropriate user context and
- * return execution to the instruction directly following the IPC
- * system call.
- */
-INLINE void tcb_t::return_from_ipc (void)
-{
-
-    register word_t utcb asm("r12") = get_local_id ().get_raw ();
-
-    asm("movq %0, %%rsp\n"
-	"retq\n"
-	:
-	:
-	"r" (&get_stack_top ()[KSTACK_RET_IPC]),
-	"r" (utcb),
-	"d" (get_tag ().raw)
-	);	
-    
-}
-
-
-/**
- * Short circuit a return path from a user-level interruption or
- * exception.  That is, restore the complete exception context and
- * resume execution at user-level.
- */
-INLINE void tcb_t::return_from_user_interruption (void)
-{
-    asm("movq %0, %%rsp\n"
-	"retq\n"
-	:
-	: "r"(&get_stack_top()[- sizeof(x86_exceptionframe_t)/8 - 1]));    
-}
 
 
 /********************************************************************** 
@@ -349,30 +184,36 @@ INLINE void tcb_t::switch_to(tcb_t * dest)
 	resources.load(this);
 }
 
-#endif
 
-
-
-INLINE word_t * tcb_t::get_stack_top()
-{
-    
-     return (word_t*)addr_offset(this, KTCB_SIZE);
-}
+/**********************************************************************
+ *
+ *                        in-kernel IPC invocation 
+ *
+ **********************************************************************/
 
 /**
- * intialize stack for given thread
+ * invoke an IPC from within the kernel
+ *
+ * @param to_tid destination thread id
+ * @param from_tid from specifier
+ * @param timeout IPC timeout
+ * @return IPC message tag (MR0)
  */
-INLINE void tcb_t::init_stack()
-{ 
-    stack = get_stack_top();
+INLINE msg_tag_t tcb_t::do_ipc (threadid_t to_tid, threadid_t from_tid,
+                                timeout_t timeout)
+{
+    msg_tag_t tag;
+   
+    sys_ipc (to_tid, from_tid, 0, 0, 0, 0, timeout);
+    tag.raw = get_mr (0);
+
+    return tag;
 }
-
-
 
 
 /**********************************************************************
  *
- *                        Notification functions
+ *                        notification functions
  *
  **********************************************************************/
 
@@ -431,68 +272,44 @@ INLINE void tcb_t::notify (void (*func)(word_t, word_t), word_t arg1, word_t arg
 }
 
 
-
-/**********************************************************************
- * 
- *            access functions for ex-regs'able registers
- *
- **********************************************************************/
-
 /**
- * read the user-level instruction pointer
- * @return	the user-level stack pointer
+ * Short circuit a return path from an IPC system call.  The error
+ * code TCR and message registers are already set properly.  The
+ * function only needs to restore the appropriate user context and
+ * return execution to the instruction directly following the IPC
+ * system call.
  */
-INLINE addr_t tcb_t::get_user_ip()
+INLINE void tcb_t::return_from_ipc (void)
 {
-    return (addr_t)get_stack_top()[KSTACK_UIP];
-}
 
-/**
- * read the user-level stack pointer
- * @return	the user-level stack pointer
- */
-INLINE addr_t tcb_t::get_user_sp()
-{
-    return (addr_t)get_stack_top()[KSTACK_USP];
-}
+    register word_t utcb asm("r12") = get_local_id ().get_raw ();
 
-/**
- * read the user-level flags (one word)
- * @return	the user-level flags
- */
-INLINE word_t tcb_t::get_user_flags (void)
-{
-    return get_stack_top()[KSTACK_UFLAGS];
-}
-
-/**
- * set the user-level instruction pointer
- * @param ip	new user-level instruction pointer
- */
-INLINE void tcb_t::set_user_ip(addr_t ip)
-{
-    get_stack_top()[KSTACK_UIP] = (u64_t)ip;
-}
-
-/**
- * set the user-level stack pointer
- * @param sp	new user-level stack pointer
- */
-INLINE void tcb_t::set_user_sp(addr_t sp)
-{
-    get_stack_top()[KSTACK_USP] = (u64_t)sp;
+    asm("movq %0, %%rsp\n"
+	"retq\n"
+	:
+	:
+	"r" (&get_stack_top ()[KSTACK_RET_IPC]),
+	"r" (utcb),
+	"d" (get_tag ().raw)
+	);	
+    
 }
 
 
-
 /**
- * set the user-level flags
- * @param flags	new user-level flags
+ * Short circuit a return path from a user-level interruption or
+ * exception.  That is, restore the complete exception context and
+ * resume execution at user-level.
  */
-INLINE void tcb_t::set_user_flags (const word_t flags)
+INLINE void tcb_t::return_from_user_interruption (void)
 {
-    get_stack_top()[KSTACK_UFLAGS] = (get_user_flags() & (~X86_USER_FLAGMASK)) | (flags & X86_USER_FLAGMASK);
+    asm("movq %0, %%rsp\n"
+	"retq\n"
+	:
+	: "r"(&get_stack_top()[- sizeof(x86_exceptionframe_t)/8 - 1]));    
 }
+
+
 
 /**********************************************************************
  *
@@ -500,25 +317,6 @@ INLINE void tcb_t::set_user_flags (const word_t flags)
  *
  **********************************************************************/
 
-/**
- * Enable copy area for current thread.
- *
- * @param dst		destination TCB for IPC copy operation
- * @param saddr		source address
- * @param daddr		destination address
- */
-INLINE void tcb_t::adjust_for_copy_area (tcb_t * dst, addr_t * saddr, addr_t * daddr)
-{
-    resources.enable_copy_area (this, saddr, dst, daddr);
-}
-
-/**
- * Release copy area(s) for current thread.
- */
-INLINE void tcb_t::release_copy_area (void)
-{
-    resources.release_copy_area (this, true); 
-}
 
 /**
  * Retrieve the real address associated with a copy area address.
@@ -537,50 +335,23 @@ INLINE addr_t tcb_t::copy_area_real_address (addr_t addr)
                          (word_t) addr & (COPY_AREA_SIZE-1));
 }
 
+#endif /* !defined(BUILD_TCB_LAYOUT) */
+
 /**********************************************************************
  *
  *                        global tcb functions
  *
  **********************************************************************/
 
-INLINE tcb_t * addr_to_tcb (addr_t addr)
-{
-    return (tcb_t *) ((word_t) addr & KTCB_MASK);
-}
 
 INLINE tcb_t * get_current_tcb()
 {
     addr_t stack;
     asm ("leaq -8(%%rsp), %0" :"=r" (stack));
-    return addr_to_tcb (stack);
+    return (tcb_t *) ((word_t) stack & KTCB_MASK);
     
 }
 
-#ifdef CONFIG_SMP
-INLINE cpuid_t get_current_cpu()
-{
-  return get_idle_tcb()->get_cpu();
-}
-#endif
-
-/**
- * invoke an IPC from within the kernel
- *
- * @param to_tid destination thread id
- * @param from_tid from specifier
- * @param timeout IPC timeout
- * @return IPC message tag (MR0)
- */
-INLINE msg_tag_t tcb_t::do_ipc (threadid_t to_tid, threadid_t from_tid,
-                                timeout_t timeout)
-{
-    msg_tag_t tag;
-    
-    sys_ipc (to_tid, from_tid, 0, 0, 0, 0, timeout);
-    tag.raw = get_mr (0);
-
-    return tag;
-}
 
 /**********************************************************************
  *
