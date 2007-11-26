08ecf3e18f419b53b48af171f975f14b93ff0048
Jan Stoess <stoess@ira.uka.de>
2007-11-26 11:18:34 +0100

    - bitmask_t is a template now

--- kernel/src/api/v4/resources.h
+++ kernel/src/api/v4/resources.h
@@ -1,6 +1,6 @@
 /*********************************************************************
  *                
- * Copyright (C) 2002, 2003,  Karlsruhe University
+ * Copyright (C) 2002, 2003, 2007,  Karlsruhe University
  *                
  * File path:     api/v4/resources.h
  * Description:   
@@ -61,7 +61,7 @@ typedef word_t	resource_bits_t;
  */
 class resource_bits_t
 {
-    bitmask_t	resource_bits;
+    bitmask_t<word_t>	resource_bits;
 
 public:
 
