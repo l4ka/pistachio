122a6750f47ddff8446655e6e752ded1664dbd27
Jan Stoess <stoess@ira.uka.de>
2007-11-26 13:19:59 +0100

    - Merge init_bootmem() and add_more_kmem() from x32/x64 - Beautify  debug output during initialization - Print L4 memory layour during initialization - Rename APIC_MAPPINGS to APIC_MAPPINGS_START and X86_4KPAGE to X86_PAGE

--- kernel/src/glue/v4-x86/init.cc
+++ kernel/src/glue/v4-x86/init.cc
@@ -34,6 +34,7 @@
 #include <ctors.h>
 #include <init.h>
 #include <linear_ptab.h>
+#include <kdb/tracepoints.h>
 
 #include INC_GLUE(idt.h)
 #include INC_GLUE(intctrl.h)
@@ -45,6 +46,7 @@
 #include INC_ARCH(apic.h)
 #include INC_ARCH(amdhwcr.h)
 
+#include INC_API(smp.h)
 #include INC_API(kernelinterface.h)
 #include INC_API(processor.h)
 #include INC_API(schedule.h)
@@ -58,12 +60,10 @@
 
 
 // from either glue/v4-ia32/ or glue/v4-x86/
-void add_more_kmem();
 void setup_msrs();
 void SECTION(SEC_INIT) init_meminfo();
 void SECTION(".init.cpu") check_cpu_features();
 cpuid_t SECTION(".init.cpu") init_cpu();
-void SECTION(SEC_INIT) init_bootmem();
 void SECTION(SEC_INIT) setup_gdt(x86_tss_t &tss, cpuid_t cpuid);
 
 #if defined(CONFIG_TRACEBUFFER)
@@ -103,7 +103,7 @@ spinlock_t smp_commence_lock;
 
 INLINE u8_t get_apic_id (void)
 {
-    local_apic_t<APIC_MAPPINGS> apic;
+    local_apic_t<APIC_MAPPINGS_START> apic;
     return apic.id();
 }
 
@@ -136,9 +136,9 @@ static void smp_bp_commence (void)
  */
 extern "C" void SECTION(SEC_INIT) startup_processor (void)
 {
-    TRACE_INIT("AP processor is alive\n");
-    x86_mmu_t::set_active_pagetable((word_t)get_kernel_space()->get_pagetable());
-    TRACE_INIT("AP switched to kernel ptab\n");
+    TRACE_INIT("\tAP processor is alive\n");
+    x86_mmu_t::set_active_pagetable((word_t) get_kernel_space()->get_top_pdir_phys());
+    TRACE_INIT("\tAP switched to kernel ptab\n");
 
     // first thing -- check CPU features
     check_cpu_features();
@@ -162,6 +162,108 @@ void SECTION(SEC_INIT) clear_bss (void)
     extern u8_t _end_bss[];
     for (u8_t* p = _start_bss; p < _end_bss; p++)
 	*p = 0;
+}
+
+/**
+ * init_bootmem: initializes the boot memory
+ *
+ * At system startup, a fixed amount of kernel memory is allocated
+ * to allow basic initialization of the system before sigma0 is up.
+ */
+
+void SECTION(SEC_INIT) init_bootmem (void)
+{
+    for (addr_t p = start_bootmem; p < end_bootmem; p = addr_offset(p, sizeof(word_t)))
+	* (word_t *)p = 0;
+
+    kmem.init(start_bootmem, end_bootmem);
+    /* now do reservations */
+
+    // Mark the kernel code as reserved
+    get_kip()->reserved_mem0.set(start_text_phys, end_bootmem_phys);
+
+#if defined(CONFIG_SUBARCH_X32)
+    // Were we booted via RMGR?
+    if (!get_kip()->main_mem.is_empty())
+    {
+        word_t end = (word_t)get_kip()->main_mem.high;
+
+        /* Allocate from end of physical memory or from end of kernel
+         *accessible physical memory, whatever is lower  */
+	if (end < virt_to_phys (KERNEL_AREA_END))
+	    end = virt_to_phys(KERNEL_AREA_END);
+        
+        get_kip()->reserved_mem1.set((addr_t) (end - ADDITIONAL_KMEM_SIZE),
+                                     (addr_t) end);
+    }
+#endif
+
+}
+
+void SECTION(SEC_INIT) add_more_kmem (void)
+{
+    /* Scan memory descriptors for a block of reserved physical memory
+     * that is within the range of (to be) contiguously mapped
+     * physical memory.  If there's one, it has been set up by the
+     * boot loader for us. */
+    bool found = false;
+    for (word_t i = 0;
+         i < get_kip()->memory_info.get_num_descriptors();
+         i++)
+    {
+        memdesc_t* md = get_kip()->memory_info.get_memdesc(i);
+        if (!md->is_virtual() &&
+            (md->type() == memdesc_t::reserved) &&
+            (word_t) md->high() <= KERNEL_AREA_END)
+        {
+	    // If KMEM is larger than 32 MB, allocate it chunkwise
+	    addr_t low = md->low();
+	    word_t size = md->size();
+	    const word_t chunksize = 32 * 1024 * 1024;
+	    
+	    while (size) 
+	    {
+		if (size >= chunksize )
+		{
+		    // Map region kernel writable 
+		    get_kernel_space()->remap_area(
+			phys_to_virt(low), low,
+			PGSIZE_KERNEL,
+			chunksize, true, true, true);	    
+		    
+		    // Add it to allocator
+		    kmem.add(phys_to_virt(low), chunksize);
+		    
+		    low = addr_offset(low, chunksize);
+		    size -= chunksize;
+		}
+		else if (size &= ~(KERNEL_PAGE_SIZE-1))
+		{
+		    // Map region kernel writable 
+		    get_kernel_space()->remap_area(
+			phys_to_virt(low), low,
+			PGSIZE_KERNEL,
+			size, true, true, true);
+		    
+		    // Add it to allocator
+		    kmem.add(phys_to_virt(low), size);
+		    size = 0;
+		}
+	    }
+            found = true;
+        }
+    }
+    
+    /* Fall back to ol'style if no memory descriptors can be
+       found */
+    if (!found)
+    {
+        if (!get_kip()->reserved_mem1.is_empty())
+        {
+            kmem.add(phys_to_virt(get_kip()->reserved_mem1.low), 
+                     get_kip()->reserved_mem1.get_size());
+        }
+    }
 }
 
 
@@ -193,53 +295,53 @@ cpuid_t SECTION(".init.cpu") init_cpu (void)
 
     call_cpu_ctors();
 
-    TRACE_INIT("Activating TSS (CPU %d)\n", cpuid);
+    TRACE_INIT("\tActivating TSS (CPU %d)se\n", cpuid);
     tss.setup(X86_KDS);
 
-    TRACE_INIT("Initializing GDT (CPU %d)\n", cpuid);
+    TRACE_INIT("\tInitializing GDT (CPU %d)\n", cpuid);
     setup_gdt(tss, cpuid);
 
     /* can take exceptions from now on,
      * idt is initialized via a constructor */
-    TRACE_INIT("Activating IDT (CPU %d)\n", cpuid);;
+    TRACE_INIT("\tActivating IDT (CPU %d)\n", cpuid);;
     idt.activate();
 
 #if defined(CONFIG_PERFMON)
 
 #if defined(CONFIG_TBUF_PERFMON)
     /* initialize tracebuffer */
-    TRACE_INIT("Initializing Tracebuffer PMCs (CPU %d)\n", cpuid);
+    TRACE_INIT("\tInitializing Tracebuffer PMCs (CPU %d)\n", cpuid);
     setup_perfmon_cpu(cpuid);
 #endif
 
      /* Allow performance counters for users */
-    TRACE_INIT("Enabling performance monitoring at user level (CPU %d)\n", cpuid);
+    TRACE_INIT("\tEnabling performance monitoring at user level (CPU %d)\n", cpuid);
     x86_cr4_set(X86_CR4_PCE);
 #endif /* defined(CONFIG_PERFMON) */
 
 #if defined(CONFIG_X86_PGE)
-    TRACE_INIT("Enabling global pages (CPU %d)\n", cpuid);
+    TRACE_INIT("\tEnabling global pages (CPU %d)\n", cpuid);
     x86_mmu_t::enable_global_pages();
 #endif
 
 #if defined(CONFIG_CPU_X86_K8) 
 #if defined(CONFIG_K8_FLUSHFILTER)
-    TRACE_INIT("Enabling K8 Flush Filter\n");
+    TRACE_INIT("\tEnabling K8 Flush Filter\n");
     x86_amdhwcr_t::enable_flushfilter();
 #else
-    TRACE_INIT("Disabling K8 Flush Filter\n");
-    x86_amdhwcr_t::disable_flushfilter();
+    TRACE_INIT("\tDisabling K8 Flush Filter\n");
+    //x86_amdhwcr_t::disable_flushfilter();
 #endif
 #endif
 
-    TRACE_INIT("Activating MSRS (CPU %d)\n", cpuid);
+    TRACE_INIT("\tActivating MSRS (CPU %d)\n", cpuid);
     setup_msrs();
 
-    TRACE_INIT("Initializing Timer (CPU %d)\n", cpuid);
-    get_timer()->init_cpu();
+    TRACE_INIT("\tInitializing Timer (CPU %d)\n", cpuid);
+    get_timer()->init_cpu(cpuid);
 
     /* initialize V4 processor info */
-    TRACE_INIT("Initializing Processor (CPU %d)\n", cpuid);
+    TRACE_INIT("\tInitializing Processor (CPU %d)\n", cpuid);
     init_processor (cpuid, get_timer()->get_bus_freq(),
 		    get_timer()->get_proc_freq());
 
@@ -291,13 +393,34 @@ extern "C" void SECTION(".init.init64") startup_system(u32_t is_ap)
     TRACE_INIT("Checking CPU features\n");
     check_cpu_features();
 
+    TRACE_INIT("virtual memory layout:\n"
+	       "\tuser area     %wx - %wx\n"
+	       "\tcopy area     %wx - %wx\n"
+	       "\tktcb area     %wx - %wx\n"
+	       "\tkernel area   %wx - %wx\n"
+	       "\tcpulocal data %wx - %wx\n"
+#if defined(CONFIG_IOAPIC)
+	       "\tapic area     %wx - %wx\n"
+#endif
+	       "\tutcb pgarea   %wx\n"
+	       "\tspace link    %wx\n",
+	       USER_AREA_START, USER_AREA_END,
+	       COPY_AREA_START, COPY_AREA_END,
+	       KTCB_AREA_START, KTCB_AREA_END,
+	       KERNEL_AREA_START, KERNEL_AREA_END,
+	       start_cpu_local, end_cpu_local,
+#if defined(CONFIG_IOAPIC)
+	       APIC_MAPPINGS_START, APIC_MAPPINGS_END,
+#endif
+	       UTCB_MAPPING,  SPACE_BACKLINK
+	);
+	       
+	       
     /* feed the kernel memory allocator */
-    TRACE_INIT("Initializing boot memory (%p - %p)\n",
-	       start_bootmem, end_bootmem);
     init_bootmem();
 
     TRACE_INIT("Initializing kernel space\n");
-    init_kernel_space();
+    space_t::init_kernel_space();
 
     TRACE_INIT("Activating TSS (Preliminary)\n");
     tss.setup(X86_KDS);
@@ -359,14 +482,14 @@ extern "C" void SECTION(".init.init64") startup_system(u32_t is_ap)
 	init_xcpu_handling ();
 
 	// copy startup code to startup page
-	for (word_t i = 0; i < X86_4KPAGE_SIZE / sizeof(word_t); i++)
+	for (word_t i = 0; i < X86_PAGE_SIZE / sizeof(word_t); i++)
 	    ((word_t*)SMP_STARTUP_ADDRESS)[i] = ((word_t*)_start_ap)[i];
 
 	/* at this stage we still have our 1:1 mapping at 0 */
 	*((volatile unsigned short *) 0x469) = (SMP_STARTUP_ADDRESS >> 4);
 	*((volatile unsigned short *) 0x467) = (SMP_STARTUP_ADDRESS) & 0xf;
 
-	local_apic_t<APIC_MAPPINGS> local_apic;
+	local_apic_t<APIC_MAPPINGS_START> local_apic;
 
 	word_t apic_id = get_apic_id();
 
@@ -391,7 +514,6 @@ extern "C" void SECTION(".init.init64") startup_system(u32_t is_ap)
 #endif /* CONFIG_SMP */
 
     /* Initialize CPU */
-    TRACE_INIT("Initializing CPU 0\n");
     cpuid_t cpuid = init_cpu();
     
 #ifdef CONFIG_SMP
