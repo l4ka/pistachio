87d49f45fdab0ae43aa647ec1fe769ae8d30a6f4
Tyson Key <tyson.key@gmail.com>
2014-05-08 18:16:13 +0100

    Try importing Shiritori game from http://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1390514758

--- user/apps/counter/counter.cc
+++ user/apps/counter/counter.cc
@@ -71,7 +71,40 @@ int Beep() {
 
 }
 
+//http://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1390514758
+int ShiritoriGame() {
+/*
+#include <stdio.h>
+#include <string.h>
 
+#define MAX 30
+
+int main(int argc, char *argv[]){
+char data[MAX][200] = {0};*/ /* 入力データ保存用 */
+/*int i,nIdx,n;
+
+for(i=0;i<MAX;i++){
+printf("%02d番目：",i+1);
+fgets(data[i], sizeof(data[i]), stdin);
+n=strlen(data[i])-3; */ /* 文末の位置 */
+/*if( memcmp( &data[i][n], "ん", 2) == 0){
+printf("んが付いたから終了\n" );
+break;
+}
+if( i!=0 && *//* 初回は比較しない */ 
+/*memcmp( &data[i-1][nIdx], &data[i][0], 2) != 0){*/ /* 今回の文頭と前回の文末を比較*/
+printf("つながらないから終了\n" );
+break;;
+}
+nIdx =n;
+}
+if(i==MAX){
+printf("%d回で終了\n",MAX );
+}
+return 0;
+}
+ */
+}
 
 int main (void) {
 
@@ -93,7 +126,7 @@ if (obsd_strcmp(iEnvStatus[ACTIVE_CMD], "beep") == 0)
 printf("Entered beep\n");
 	iEnvStatus[CMD_RESULT] = (char*)Beep();
 	
-	printf("\n%D\n\n", (char*)iEnvStatus[CMD_RESULT]);
+	printf("\n%d\n\n", (char*)iEnvStatus[CMD_RESULT]);
 
 	//Return to prompt
 	iEnvStatus[CMD_RESULT] = (char*)WAITING;
