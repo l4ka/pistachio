8b395a7310cd206739b9acf623fcb7f45ebe4fd2
Tyson Key <tyson.key@gmail.com>
2014-05-11 19:08:12 +0100

    Plug liballoc into build system

--- libcxxrt/src/tobuild.sh
+++
@@ -1,1 +0,0 @@
-gcc -x c++ -I../../include -I../../ -I/usr/lib/gcc/i686-linux-gnu/4.8/include   -fno-stack-protector -nostdinc -g -O2 -m32 -O2 -g -Wall -Wshadow  -Wno-conversion -fexceptions src/*.c src/*.cc
