f43a2ab16c82643e5e4e8e9475f2cba96e1107c1
Tyson Key <tyson.key@gmail.com>
2014-05-12 08:38:05 +0100

    Notes on hooks

--- user/lib/io/liballoc_hooks.c
+++ user/lib/io/liballoc_hooks.c
@@ -1,4 +1,8 @@
 #include <liballoc.h>
+#include <l4/space.h>
+
+//http://www.cse.unsw.edu.au/~cs9242/05/project/l4uman-x2.pdf
+//https://lists.ira.uni-karlsruhe.de/pipermail/l4ka/2008-April/002072.html
 
 /** This function is supposed to lock the memory data structures. It
  * could be as simple as disabling interrupts or acquiring a spinlock.
@@ -21,6 +25,50 @@ extern int liballoc_unlock() {
 return 0;
 }
 
+
+void *
+la_get_pages( L4_Word_t count, int touch )
+{ 
+
+
+	void *ret = free_page;
+
+	free_page += count * PAGE_SIZE; 
+	
+	/* should we fault the pages in? */
+	if( touch != 0 )
+	{
+		char *addr = (char*) ret;
+		L4_Word_t i;
+
+		/* touch each page */
+		for( i = 0; i < count; i++ )
+		{
+			la_safe_mem_touch( (void*) addr );
+			for (int j=0; j<PAGE_SIZE; j++)
+			    addr[j] = 0;
+				
+			addr += PAGE_SIZE;
+			
+		}
+	}
+
+	return (void*) ret;
+}
+
+L4_Word_t
+la_safe_mem_touch( void *addr )
+{
+	volatile L4_Word_t *ptr;
+	L4_Word_t copy;
+
+	ptr = (L4_Word_t*) addr;
+	copy = *ptr;
+	*ptr = copy;
+
+	return copy;
+}
+
 /** This is the hook into the local system which allocates pages. It
  * accepts an integer parameter which is the number of pages
  * required.  The page size was set up in the liballoc_init function.
@@ -28,9 +76,11 @@ return 0;
  * \return NULL if the pages were not allocated.
  * \return A pointer to the allocated memory.
  */
-extern void* liballoc_alloc(int aPagesRequird) {
+extern void* liballoc_alloc(int aPagesReq) {
 
-return NULL;
+return la_get_pages(aPagesReq, 0);
+
+//return NULL;
 }
 
 /** This frees previously allocated memory. The void* parameter passed
@@ -41,7 +91,11 @@ return NULL;
  *
  * \return 0 if the memory was successfully freed.
  */
+//L4_INLINE void L4_Unmap (L4_Word_t n, L4_Fpage_t * fpages)
 extern int liballoc_free(void* aPrevCallVal,int aPagesToFree) {
+//L4_Unmap(aPagesToFree,aPrevCallVal);
+L4_Unmap(aPagesToFree);
+
 return 0;
 }
 
