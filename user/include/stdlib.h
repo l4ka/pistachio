e9e3794cb698d007848bbb838e5d28fb1c286e4a
Tyson Key <tyson.key@gmail.com>
2014-05-18 00:27:02 +0100

    Update Makefile, and stlib.h to export l64a APIs

---
+++ user/include/stdlib.h
@@ -0,0 +1,18 @@
+#ifndef __STDLIB_H__
+#define __STDLIB_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char * l64a(long value);
+int l64a_r(long value, char *buffer, int buflen);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* !__STDLIB_H__ */
+
