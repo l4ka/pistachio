9d25e52c37c81451dbfa55373778ffc5f5530a6e
Tyson Key <tyson.key@gmail.com>
2014-05-18 00:45:42 +0100

    Hacky (u)intptr_t. Is this correct? But bsearch builds, now...

--- user/include/sys/cdefs.h
+++ user/include/sys/cdefs.h
@@ -3,3 +3,11 @@
 #ifndef	__DECONST
 #define	__DECONST(type, var)	((type)(uintptr_t)(const void *)(var))
 #endif
+
+//HAX! - http://www.opensource.apple.com/source/gm4/gm4-15/lib/stdint_.h
+//These will probably break things badly, in the future
+
+#undef intptr_t
+#undef uintptr_t
+#define intptr_t long int
+#define uintptr_t unsigned long int
