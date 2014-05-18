7cefe9e4da7eb15f50c37f19b63d46c749e5dc09
Tyson Key <tyson.key@gmail.com>
2014-05-18 22:47:07 +0100

    Quick-and-dirty port of the ToARu PC speaker driver from https://raw.githubusercontent.com/klange/toaruos/strawberry-dev/modules/pcspkr.c. License is https://github.com/klange/toaruos/blob/strawberry-dev/LICENSE.md (NCSA).

---
+++ user/apps/counter/pcspkr.c
@@ -0,0 +1,70 @@
+//#include <module.h>
+//#include <printf.h>
+//#include <mod/shell.h>
+#include <stdio.h>
+#include <lib/io/ia32.h>
+#define outportb outb
+#define inportb inb
+#define uint8_t int
+#define uint32_t unsigned long
+static void note(int length, int freq) {
+
+	uint32_t div = 11931800 / freq;
+	uint8_t  t;
+
+
+	outportb(0x43, 0xb6);
+	outportb(0x42, (uint8_t)(div));
+	outportb(0x42, (uint8_t)(div >> 8));
+
+	t = inportb(0x61);
+	outportb(0x61, t | 0x3);
+
+	unsigned long s, ss;
+	//relative_time(0, length, &s, &ss);
+	//sleep_until((process_t *)current_process, s, ss);
+	//switch_task(0);
+
+	t = inportb(0x61) & 0xFC;
+	outportb(0x61, t);
+
+}
+
+void TetrisTheme() {
+	//fprintf(tty, "beep\n");
+	printf("beep\n");
+
+	note(20, 15680);
+	note(10, 11747);
+	note(10, 12445);
+	note(20, 13969);
+	note(10, 12445);
+	note(10, 11747);
+	note(20, 10465);
+	note(10, 10465);
+	note(10, 12445);
+	note(20, 15680);
+	note(10, 13969);
+	note(10, 12445);
+	note(30, 11747);
+	note(10, 12445);
+	note(20, 13969);
+	note(20, 15680);
+	note(20, 12445);
+	note(20, 10465);
+	note(20, 10465);
+
+	//return 0;
+}
+
+/*static int init(void) {
+	BIND_SHELL_FUNCTION(beep);
+	return 0;
+}
+
+static int fini(void) {
+	return 0;
+}*/
+
+//MODULE_DEF(pcspkr, init, fini);
+//MODULE_DEPENDS(debugshell);
