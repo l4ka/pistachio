7d7869da81ae426d373a51ceb5ce2d5a296437a2
Tyson Key <tyson.key@gmail.com>
2014-05-19 16:23:02 +0100

    Define getpagesize, but don't implement

--- user/include/unistd.h
+++ user/include/unistd.h
@@ -13,6 +13,10 @@ extern "C" {
 //BSD swab
 void swab(const void *from, void *to, size_t len);
 
+//http://man7.org/linux/man-pages/man2/getpagesize.2.html
+//OSF/1, Linux getpagesize()
+int getpagesize(void);
+
 #ifdef __cplusplus
 }
 #endif
