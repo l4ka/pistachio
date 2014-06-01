44d3488728e98e346292b82ee0ecd87a69f3733c
Tyson Key <tyson.key@gmail.com>
2014-06-01 23:34:38 +0100

    Basic NWGetPageSize

---
+++ user/lib/io/netware/apicore.c
@@ -0,0 +1,13 @@
+#include <nwmalloc.h> 
+    
+//https://www.novell.com/documentation/developer/clib/prog_enu/data/sdk645.html says that this should usually return  4096 bytes, but that feels wrong on this platform
+  size_t NWGetPageSize  (void) {
+
+//HACK based on example code, as used in our liballoc port...
+#define PAGE_BITS		(12)
+#define PAGE_SIZE		(1 << PAGE_BITS)
+#define SCRATCHMEM_START        (16*1024*1024)
+
+return PAGE_SIZE;
+
+}
