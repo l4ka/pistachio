2fd1c5b20b31b9e8413377e389ddcfd28a6287c7
Tyson Key <tyson.key@gmail.com>
2014-06-01 23:48:35 +0100

    Plug in the API core

--- user/lib/io/netware/apicore.c
+++ user/lib/io/netware/apicore.c
@@ -1,6 +1,12 @@
 #include <nwmalloc.h> 
     
 //https://www.novell.com/documentation/developer/clib/prog_enu/data/sdk645.html says that this should usually return  4096 bytes, but that feels wrong on this platform
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
   size_t NWGetPageSize  (void) {
 
 //HACK based on example code, as used in our liballoc port...
@@ -11,3 +17,9 @@
 return PAGE_SIZE;
 
 }
+
+
+#ifdef __cplusplus
+}
+#endif
+
