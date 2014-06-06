5b4aded2d4b6742f601d06cd0d9d43d97d120fa4
Tyson Key <tyson.key@gmail.com>
2014-06-06 20:28:23 +0100

    Check in ShedSkin examples temporarily

---
+++ user/lib/io/hello.hpp
@@ -0,0 +1,16 @@
+#ifndef __HELLO_HPP
+#define __HELLO_HPP
+
+using namespace __shedskin__;
+namespace __hello__ {
+
+extern str *const_0;
+
+
+
+extern str *__name__;
+
+
+
+} // module namespace
+#endif
