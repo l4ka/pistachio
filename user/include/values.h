aaa018f4f216c563f7f3bad55b542d7a1403a6f0
Tyson Key <tyson.key@gmail.com>
2014-06-10 03:32:00 +0100

    Touch up values.h definitions

--- user/include/values.h
+++ user/include/values.h
@@ -1,10 +1,6 @@
 //Probably varies per CPU
 #define BITSPERBYTE 8 
 
-void sputl(l, buffer)
-register long l;
-register char *buffer;
-
-long  sgetl(buffer)
-register char *buffer;
+void sputl(register long l, register char *buffer);
+long  sgetl(register char *buffer);
 
