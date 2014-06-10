e55f8bcf8f48fc39d4e4836ba79ea95906540198
Tyson Key <tyson.key@gmail.com>
2014-06-10 17:13:13 +0100

    Additional type definitions

--- user/include/sys/types.h
+++ user/include/sys/types.h
@@ -13,10 +13,12 @@ extern "C" {
 //http://fxr.watson.org/fxr/source/sys/types.h?v=OPENBSD
 typedef unsigned int    u_int;
 
+
 #define	RAND_MAX	2147483647 // Like Linux
 
+typedef unsigned long long u_quad_t;
 typedef long long quad_t;
-
+typedef L4_Word_t u_long;
 void * memset(void *s1, int c, size_t n);
 size_t strlen(const char *str);
 
