5acdd380c19d84aefe0401bf75266d4ac9086475
Tyson Key <tyson.key@gmail.com>
2014-09-07 22:04:05 +0100

    Import ftp://ftp.fr.openbsd.org/pub/OpenBSD/src/lib/libc/gdtoa/strtord.c

--- user/lib/io/bsd/gdtoa/strtord.c
+++ user/lib/io/bsd/gdtoa/strtord.c
@@ -31,81 +31,54 @@ THIS SOFTWARE.
 
 #include "gdtoaimp.h"
 
-#undef _0
-#undef _1
-
-/* one or the other of IEEE_MC68k or IEEE_8087 should be #defined */
-
-#ifdef IEEE_MC68k
-#define _0 0
-#define _1 1
-#define _2 2
-#define _3 3
-#define _4 4
-#endif
-#ifdef IEEE_8087
-#define _0 4
-#define _1 3
-#define _2 2
-#define _3 1
-#define _4 0
-#endif
-
  void
 #ifdef KR_headers
-ULtox(L, bits, exp, k) UShort *L; ULong *bits; Long exp; int k;
+ULtod(L, bits, exp, k) ULong *L; ULong *bits; Long exp; int k;
 #else
-ULtox(UShort *L, ULong *bits, Long exp, int k)
+ULtod(ULong *L, ULong *bits, Long exp, int k)
 #endif
 {
 	switch(k & STRTOG_Retmask) {
 	  case STRTOG_NoNumber:
 	  case STRTOG_Zero:
-		L[0] = L[1] = L[2] = L[3] = L[4] = 0;
+		L[0] = L[1] = 0;
 		break;
 
 	  case STRTOG_Denormal:
-		L[_0] = 0;
-		goto normal_bits;
+		L[_1] = bits[0];
+		L[_0] = bits[1];
+		break;
 
 	  case STRTOG_Normal:
 	  case STRTOG_NaNbits:
-		L[_0] = exp + 0x3fff + 63;
- normal_bits:
-		L[_4] = (UShort)bits[0];
-		L[_3] = (UShort)(bits[0] >> 16);
-		L[_2] = (UShort)bits[1];
-		L[_1] = (UShort)(bits[1] >> 16);
+		L[_1] = bits[0];
+		L[_0] = (bits[1] & ~0x100000) | ((exp + 0x3ff + 52) << 20);
 		break;
 
 	  case STRTOG_NoMemory:
-//		errno = ERANGE;
+		errno = ERANGE;
 		/* FALLTHROUGH */
 	  case STRTOG_Infinite:
-		L[_0] = 0x7fff;
-		L[_1] = 0x8000;
-		L[_2] = L[_3] = L[_4] = 0;
+		L[_0] = 0x7ff00000;
+		L[_1] = 0;
 		break;
 
 	  case STRTOG_NaN:
-		L[0] = ldus_QNAN0;
-		L[1] = ldus_QNAN1;
-		L[2] = ldus_QNAN2;
-		L[3] = ldus_QNAN3;
-		L[4] = ldus_QNAN4;
+		L[0] = d_QNAN0;
+		L[1] = d_QNAN1;
 	  }
 	if (k & STRTOG_Neg)
-		L[_0] |= 0x8000;
+		L[_0] |= 0x80000000L;
 	}
 
  int
 #ifdef KR_headers
-strtorx(s, sp, rounding, L) CONST char *s; char **sp; int rounding; void *L;
+strtord(s, sp, rounding, d) CONST char *s; char **sp; int rounding; double *d;
 #else
-strtorx(CONST char *s, char **sp, int rounding, void *L)
+strtord(CONST char *s, char **sp, int rounding, double *d)
 #endif
 {
-	static FPI fpi0 = { 64, 1-16383-64+1, 32766 - 16383 - 64 + 1, 1, SI };
+	static FPI fpi0 = { 53, 1-1023-53+1, 2046-1023-53+1, 1, SI };
 	FPI *fpi, fpi1;
 	ULong bits[2];
 	Long exp;
@@ -118,6 +91,6 @@ strtorx(CONST char *s, char **sp, int rounding, void *L)
 		fpi = &fpi1;
 		}
 	k = strtodg(s, sp, fpi, &exp, bits);
-	ULtox((UShort*)L, bits, exp, k);
+	ULtod((ULong*)d, bits, exp, k);
 	return k;
 	}
