b4e4efcef2d530c20f4e2df2b7c224f78ae2c95b
Tyson Key <tyson.key@gmail.com>
2014-09-13 21:41:21 +0100

    Debugging

--- user/apps/counter/elmfat/src/diskio.c
+++ user/apps/counter/elmfat/src/diskio.c
@@ -145,7 +145,7 @@ reg_pio_data_in_lba28(CB_DH_DEV0,CMD_READ_SECTORS, 0x00, count, sector, buff, co
 
 		//result = ATA_disk_read(buff, sector, count);
 		result = reg_pio_data_in_lba28(CB_DH_DEV0,CMD_READ_SECTORS, 0x00, count, sector, buff, count, count);
-
+		printf("\n\n[diskio] : Buffer contained : %x\n\n", buff);
 		// translate the reslut code here
 
 		return res;
