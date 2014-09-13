5b1a1fffe4de7e7846f2986f3047497b3e0f5f8e
Tyson Key <tyson.key@gmail.com>
2014-09-13 16:36:06 +0100

    Symbian types

---
+++ user/include/e32std.h
@@ -0,0 +1,11 @@
+
+//We don't have DLLs
+#define EXPORT_C 
+
+
+//These are supposed to be defined in e32def.h, in Symbian OS
+typedef unsigned int TUint;
+
+//Real number types
+typedef double TReal;
+typedef double TReal64;
