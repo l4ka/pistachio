a095ea17024939d1a279bea9ffd19c3e847dd1e9
Tyson Key <tyson.key@gmail.com>
2014-09-13 16:41:55 +0100

    More types

--- user/include/e32std.h
+++ user/include/e32std.h
@@ -1,3 +1,4 @@
+//http://developer.nokia.com/community/wiki/Fundamental_Types_in_symbian
 
 //We don't have DLLs
 #define EXPORT_C 
@@ -5,6 +6,14 @@
 
 //These are supposed to be defined in e32def.h, in Symbian OS
 typedef unsigned int TUint;
+typedef long int TInt32;
+
+//Symbian ints are always signed, unless marked as unsigned explicitly
+typedef signed int TInt;
+
+//Compatibility value...
+typedef long long Int64;
+typedef	Int64	TInt64;
 
 //Real number types
 typedef double TReal;
