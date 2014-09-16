061bf0c72b5281a71557c1656ace07468013e3d8
Tyson Key <tyson.key@gmail.com>
2014-09-16 19:26:49 +0100

    Ugly compromise based on empty struct for FILE

--- user/include/stdio.h
+++ user/include/stdio.h
@@ -9,10 +9,26 @@
 extern "C" {
 #endif
 
+//https://github.com/haiku/haiku/blob/449f7f5a7b9d381c1eeab3ddfbdba0db0ce05c22/headers/posix/stdio.h
+//ftp://ftp.fr.openbsd.org/pub/OpenBSD/src/include/stdio.h defines FILE as a structure
+
+
+//Do like OpenBSD for now...
+typedef	struct __sFILE {
+} FILE;
+
+extern FILE __sF[];
+
+#define	stdin	(&__sF[0])
+#define	stdout	(&__sF[1])
+#define	stderr	(&__sF[2])
+//End
+
+//Haiku suggests doing the following...
 //This is a hack, we should replace it with something nicer, ASAP
-extern int *stdin;
-extern int *stdout;
-extern int *stderr;
+//extern FILE *stdin; //FILE is abstract, but what should it be?
+//extern FILE *stdout;
+//extern FILE *stderr;
 
 //Be BeOS-compatible...
 int is_computer_on();
