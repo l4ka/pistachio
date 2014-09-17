11e017f61df48cc8fcc810d8b07b521b6bd1c6f4
Tyson Key <tyson.key@gmail.com>
2014-09-18 00:10:14 +0100

    OpenVMS descriptors

---
+++ user/include/openvms/descrip.h
@@ -0,0 +1,8 @@
+/* http://h71000.www7.hp.com/commercial/c/docs/5492p012.html */
+struct  dsc$descriptor
+   {
+      unsigned  short  dsc$w_length; /*  Length of data         */
+      char  dsc$b_dtype              /*  Data type code         */
+      char  dsc$b_class              /*  Descriptor class code  */
+      char  *dsc$a_pointer           /*  Address of first byte  */
+   };
