69765888f1f60e93bcab2befa65f34977e1ac138
Tyson Key <tyson.key@gmail.com>
2014-09-17 23:14:44 +0100

    Define NULL for stdio

--- user/include/stdio.h
+++ user/include/stdio.h
@@ -22,6 +22,17 @@ typedef	struct __sFILE {
 
 extern FILE __sF[];
 
+//We've already got this, somewhere else...
+#ifndef NULL
+#ifdef 	__GNUG__
+#define	NULL	__null
+#elif defined(__cplusplus)
+#define	NULL	0L
+#else
+#define	NULL	((void *)0)
+#endif
+#endif
+
 #define	stdin	(&__sF[0])
 #define	stdout	(&__sF[1])
 #define	stderr	(&__sF[2])
