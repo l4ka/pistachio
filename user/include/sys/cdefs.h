80cf58f841178d4ac371282004b4657a8d129bf6
Tyson Key <tyson.key@gmail.com>
2014-09-20 17:19:12 +0100

    Pragma Once

--- user/include/sys/cdefs.h
+++ user/include/sys/cdefs.h
@@ -1,4 +1,4 @@
-
+#pragma once
 //https://sourceware.org/ml/newlib/2013/msg00234.html
 #ifndef	__DECONST
 #define	__DECONST(type, var)	((type)(uintptr_t)(const void *)(var))
@@ -7,15 +7,14 @@
 //HAX! - http://www.opensource.apple.com/source/gm4/gm4-15/lib/stdint_.h
 //These will probably break things badly, in the future
 
-/*
-#ifdef intptr_t
-	#undef intptr_t
+
+//#ifdef intptr_t
+//	#undef intptr_t
 	#define intptr_t long int
-#endif 
+//#endif 
 
-#ifdef uintptr_t
-	#undef uintptr_t
+//#ifdef uintptr_t
+	//#undef uintptr_t
 	#define uintptr_t unsigned long int
-#endif
+//#endif
 
-*/
