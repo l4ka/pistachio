9bdaa26202b00d356f72329cd8666b8a15238f9c
Tyson Key <tyson.key@gmail.com>
2014-09-20 20:25:05 +0100

    Create a SysLaunch class

---
+++ user/apps/syslaunch/syslaunch.cpp
@@ -0,0 +1,5 @@
+#include "syslaunch.h"
+
+SysLaunch::SysLaunch()
+{
+}
---
+++ user/apps/syslaunch/syslaunch.h
@@ -0,0 +1,10 @@
+#ifndef SYSLAUNCH_H
+#define SYSLAUNCH_H
+
+class SysLaunch
+{
+public:
+    SysLaunch();
+};
+
+#endif // SYSLAUNCH_H
