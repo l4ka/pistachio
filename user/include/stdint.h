21b1730b727e2175dfa2b40e3958745e67a7179f
Tyson Key <tyson.key@gmail.com>
2014-09-20 16:40:53 +0100

    Finish linking by defining #define UINT64_MAX

--- user/include/stdint.h
+++ user/include/stdint.h
@@ -1,5 +1,11 @@
 
 
+//Defined for NICTA's ELF loader
+typedef	unsigned long long	uint64_t;
+//typedef unsigned int		uintptr_t;
+typedef unsigned long int uintptr_t;
+#define UINT64_MAX              0xffffffffffffffffULL
+
 //Temporary for libfmt
 typedef unsigned char		uchar;
 typedef unsigned short		ushort;
