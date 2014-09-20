ae46cbe0041231daa4adb246b2c93a6509d8ebb9
Tyson Key <tyson.key@gmail.com>
2014-09-20 20:31:41 +0100

    Initialisation boilerplate

--- user/apps/syslaunch/syslaunch.cpp
+++ user/apps/syslaunch/syslaunch.cpp
@@ -3,3 +3,17 @@
 SysLaunch::SysLaunch()
 {
 }
+
+void SysLaunch::WaitForCmd() {
+
+    while(1) {
+        EDebugPrintf("SysLaunch","Still waiting...\n");
+    }
+}
+
+int main(void) {
+    SysLaunch::SysLaunch launch;
+    launch.WaitForCmd();
+
+    return 0;
+}
--- user/apps/syslaunch/syslaunch.h
+++ user/apps/syslaunch/syslaunch.h
@@ -1,10 +1,14 @@
 #ifndef SYSLAUNCH_H
 #define SYSLAUNCH_H
 
+#include <stdio.h>
+
 class SysLaunch
 {
 public:
     SysLaunch();
+private:
+    void WaitForCmd();
 };
 
 #endif // SYSLAUNCH_H
