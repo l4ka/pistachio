a9645187f59dc184c726c7c6543802a58cfc473e
Tyson Key <tyson.key@gmail.com>
2014-09-23 15:07:38 +0100

    More debug

--- user/lib/io/efile/ramfs.cc
+++ user/lib/io/efile/ramfs.cc
@@ -9,11 +9,16 @@ RamFs::RamFs()
 bool RamFs::ReadFile(char *aPath) {
 
     EDebugPrintf("RamFs", "Reading files is unimplemented");
+
+    printf("RamFs::ReadFile(%d)\n", aPath);
+
     return -1;
 }
 
 bool RamFs::WriteFile(char *aPath) {
     EDebugPrintf("RamFs", "Writing files is unsupported");
+
+    printf("RamFs::WriteFile(%d)\n", aPath);
 
     return -1;
 }
