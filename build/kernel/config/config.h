145a1f66f13988c7cfdbab029eeb464721d491c3
Tyson Key <tyson.key@gmail.com>
2014-10-03 16:12:56 +0100

    Try building kernel with keyboard + serial support

--- build/kernel/config/config.h
+++ build/kernel/config/config.h
@@ -1,6 +1,6 @@
 /* Automatically generated, don't edit */
 /* Generated on: tyson-VirtualBox */
-/* At: Fri, 16 May 2014 23:03:17 +0000 */
+/* At: Fri, 03 Oct 2014 15:11:06 +0000 */
 /* Linux version 3.11.0-12-generic (buildd@komainu) (gcc version 4.8.1 (Ubuntu/Linaro 4.8.1-10ubuntu7) ) #19-Ubuntu SMP Wed Oct 9 16:12:00 UTC 2013 */
 
 /* Pistachio Kernel Configuration System */
@@ -21,8 +21,8 @@
 /* Processor Type */
 #undef  CONFIG_CPU_X86_I486
 #undef  CONFIG_CPU_X86_I586
-#define CONFIG_CPU_X86_I686 1
-#undef  CONFIG_CPU_X86_P4
+#undef  CONFIG_CPU_X86_I686
+#define CONFIG_CPU_X86_P4 1
 #undef  CONFIG_CPU_X86_K8
 #undef  CONFIG_CPU_X86_C3
 #undef  CONFIG_CPU_X86_SIMICS
@@ -32,10 +32,27 @@
 #define CONFIG_PLAT_PC99 1
 
 
+/* Processor Type */
+#undef  CONFIG_CPU_POWERPC_PPC440
+#undef  CONFIG_CPU_POWERPC_IBM750
+#undef  CONFIG_CPU_POWERPC_PPC604
+
+
+/* Platform */
+#undef  CONFIG_PLAT_OFPPC
+#undef  CONFIG_PLAT_PPC44X
+
+
+/* Sub-Platform */
+#undef  CONFIG_SUBPLAT_440_BGP
+#undef  CONFIG_SUBPLAT_440_EBONY
+
+
 /* Miscellaneous */
 #define CONFIG_IOAPIC 1
 #define CONFIG_MAX_IOAPICS 2
 #define CONFIG_APIC_TIMER_TICK 1000
+#undef  CONFIG_PPC_EXPOSE_OPIC
 
 #undef  CONFIG_SMP
 
@@ -60,6 +77,7 @@
 #define CONFIG_SPIN_WHEELS 1
 #undef  CONFIG_NEW_MDB
 #undef  CONFIG_STATIC_TCBS
+#define CONFIG_PPC_BAT_SYSCALLS 1
 #undef  CONFIG_X86_SMALL_SPACES
 
 
@@ -69,12 +87,15 @@
 #define CONFIG_KDB_CONS_COM 1
 #define CONFIG_KDB_COMPORT 0x0
 #define CONFIG_KDB_COMSPEED 115200
-#undef  CONFIG_KDB_CONS_KBD
+#define CONFIG_KDB_CONS_KBD 1
+#undef  CONFIG_KDB_CONS_OF1275
+#undef  CONFIG_KDB_CONS_PSIM_COM
 #define CONFIG_KDB_BOOT_CONS 0
 
 #undef  CONFIG_KDB_DISAS
 #undef  CONFIG_KDB_ON_STARTUP
 #undef  CONFIG_KDB_BREAKIN
+#define CONFIG_KDB_BREAKIN_BREAK 1
 #undef  CONFIG_KDB_INPUT_HLT
 #undef  CONFIG_KDB_NO_ASSERTS
 
@@ -88,6 +109,9 @@
 
 
 /* Code Generator Options */
+#define CONFIG_SYSV_ABI 1
+#undef  CONFIG_PPC_SEGMENT_LOOP
+#define CONFIG_PPC_MULTIWORD_INSTR 1
 
 
 /* Derived symbols */
@@ -99,7 +123,7 @@
 #define CONFIG_X86_PGE 1
 #define CONFIG_X86_FXSR 1
 #define CONFIG_IS_32BIT 1
-#undef  CONFIG_X86_HTT
+#define CONFIG_X86_HTT 1
 #define CONFIG_X86_PAT 1
 #undef  CONFIG_PPC_BOOKE
 #undef  CONFIG_IS_64BIT
@@ -109,7 +133,7 @@
 #undef  CONFIG_PPC_TLB_INV_LOCAL
 #undef  CONFIG_PPC_CACHE_ICBI_LOCAL
 #undef  CONFIG_X86_SMALL_SPACES_GLOBAL
-#undef  CONFIG_X86_HVM
+#define CONFIG_X86_HVM 1
 #undef  CONFIG_PPC_MMU_SEGMENTS
 #define CONFIG_X86_TSC 1
 /* That's all, folks! */
--- build/kernel/include/version.h
+++ build/kernel/include/version.h
@@ -14,9 +14,9 @@
 #define KERNEL_SUBSUBVERSION   0			
 
 \
-#define KERNELGENDATE          SHUFFLE3(day:(118-100),month:(109-100),year:(2014-2000))			
+#define KERNELGENDATE          SHUFFLE3(day:(103-100),month:(110-100),year:(2014-2000))			
 \
 #define __USER__               "tyson@tyson-VirtualBox"				
 \
-#define __KERNELBUILDNO__      15				
+#define __KERNELBUILDNO__      16				
 
