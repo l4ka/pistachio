47cc984a98aafbe17ee1d4f2d3bc1c40f3f744a7
Tyson Key <tyson.key@gmail.com>
2014-10-12 23:38:29 +0100

    Define LDBL_MANT_DIG

--- user/include/float.h
+++ user/include/float.h
@@ -1,4 +1,6 @@
 #pragma once
 
 //http://sourceforge.jp/projects/openbsd-octeon/scm/git/openbsd-octeon/blobs/master/src/sys/arch/i386/include/float.h
+
+#define LDBL_MANT_DIG   64
 #define LDBL_MAX_EXP    16384
