728dd46280ec0311f2735ee33e2b8731cb82f7b2
Tyson Key <tyson.key@gmail.com>
2014-10-12 23:48:23 +0100

    More floaty stuff

--- user/include/float.h
+++ user/include/float.h
@@ -2,6 +2,17 @@
 
 //http://sourceforge.jp/projects/openbsd-octeon/scm/git/openbsd-octeon/blobs/master/src/sys/arch/i386/include/float.h
 
+#define FLT_ROUNDS      __flt_rounds()
+#define FLT_RADIX       2               /* b */
+
+#define DBL_DIG         15
+
+ #define DBL_MAX_10_EXP  308
+
+#define FLT_MIN_EXP     (-125)          /* emin */
+#define LDBL_MIN_EXP    (-16381)
+#define DBL_MAX_EXP     1024
+#define DBL_MANT_DIG    53
 #define DBL_MAX         1.7976931348623157E+308
 #define LDBL_MANT_DIG   64
 #define LDBL_MAX_EXP    16384
