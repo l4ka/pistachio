113e1635f64d1b2a8095cc49e4989d797309401e
Tyson Key <tyson.key@gmail.com>
2014-10-12 23:43:08 +0100

    Define DBL_MAX

--- user/include/float.h
+++ user/include/float.h
@@ -2,5 +2,6 @@
 
 //http://sourceforge.jp/projects/openbsd-octeon/scm/git/openbsd-octeon/blobs/master/src/sys/arch/i386/include/float.h
 
+#define DBL_MAX         1.7976931348623157E+308
 #define LDBL_MANT_DIG   64
 #define LDBL_MAX_EXP    16384
