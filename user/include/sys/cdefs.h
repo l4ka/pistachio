021367c9f4f7f8bce5fe0ab87e010fc1c816afb3
Tyson Key <tyson.key@gmail.com>
2014-10-19 21:07:07 +0100

    __BEGIN_DECLS stuff for BSD

--- user/include/sys/cdefs.h
+++ user/include/sys/cdefs.h
@@ -6,6 +6,16 @@
 #define	__DECONST(type, var)	((type)(uintptr_t)(const void *)(var))
 #endif
 
+//http://stackoverflow.com/questions/8087438/should-i-have-to-use-an-extern-c-block-to-include-the-c-headers
+/* C++ needs to know that types and declarations are C, not C++.  */
+#ifdef   __cplusplus
+# define __BEGIN_DECLS  extern "C" {                                            
+# define __END_DECLS }
+#else
+# define __BEGIN_DECLS
+# define __END_DECLS
+#endif
+
 //HAX! - http://www.opensource.apple.com/source/gm4/gm4-15/lib/stdint_.h
 //These will probably break things badly, in the future
 
