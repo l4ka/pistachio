ee1ec75f59f15ce5a260dd97a0cd4f8b3332b91f
Tyson Key <tyson.key@gmail.com>
2014-10-20 23:18:30 +0100

    Bits for fflush

--- user/lib/io/bsd/stdio/local.h
+++ user/lib/io/bsd/stdio/local.h
@@ -1,2 +1,12 @@
 //ftp://ftp.fr.openbsd.org/pub/OpenBSD/src/lib/libc/stdio/local.h
 extern int __sdidinit;
+
+int	__sflush(FILE *);
+int	__sflush_locked(FILE *);
+int	_fwalk(int (*)(FILE *));
+
+//http://stackoverflow.com/questions/21154211/freebsds-isthreaded-equivalent-in-linux
+//This is a hack
+#define __isthreaded 1
+#define FLOCKFILE(fp)	do { if (__isthreaded) flockfile(fp); } while (0)
+#define FUNLOCKFILE(fp)	do { if (__isthreaded) funlockfile(fp); } while (0)
