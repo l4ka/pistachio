909a84731a5f9b29e2a579f5fa54403ec8b88c15
Tyson Key <tyson.key@gmail.com>
2015-03-14 15:56:39 +0000

    Try hacking in HASUB/FREEUB

--- user/lib/io/stdio/local.h
+++ user/lib/io/stdio/local.h
@@ -11,13 +11,24 @@ extern "C" {
 
 //#include <stdio.h>
 //ftp://ftp.fr.openbsd.org/pub/OpenBSD/src/lib/libc/stdio/local.h
-
+//https://platform--bionic.android-source-browsing.googlecode.com/git/libc/stdio/local.h
 
 extern int __sdidinit;
 
 int	__sflush(FILE *);
 int	__sflush_locked(FILE *);
 int	_fwalk(int (*)(FILE *));
+
+/*
+ * Test whether the given stdio file has an active ungetc buffer;
+ * release such a buffer, without restoring ordinary unread data.
+ */
+#define  HASUB(fp) (_UB(fp)._base != NULL)
+#define  FREEUB(fp) { \
+  if (_UB(fp)._base != (fp)->_ubuf) \
+    free(_UB(fp)._base); \
+  _UB(fp)._base = NULL; \
+}
 
 //http://stackoverflow.com/questions/21154211/freebsds-isthreaded-equivalent-in-linux
 //This is a hack
