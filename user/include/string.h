09b22a3dc05425daa6ac3ba7acab605bdb2ccb84
Tyson Key <tyson.key@gmail.com>
2015-03-15 00:08:54 +0000

    Guarding

--- user/include/string.h
+++ user/include/string.h
@@ -4,7 +4,16 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <sys/types.h>
+
+#ifdef __cplusplus
+}
+#endif
+
 
 // BAD IDEA #include <lib/io/lib.h>
 
